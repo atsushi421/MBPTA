4985b916660aee84caa3cd5d9cf9302b80696737
atsushi421 <atsushi.yano.2@tier4.jp>
2023-11-17 11:43:16 +0900

    refactor

--- src/MBPTA-CV.py
+++ src/MBPTA-CV.py
@@ -1,70 +1,81 @@
 import argparse
 import os
+import sys
 
 import numpy as np
-from scipy.stats import kstest  # type: ignore
+from scipy.stats import expon, kstest  # type: ignore
 from statsmodels.stats.diagnostic import acorr_ljungbox  # type: ignore
 
-from config import (IID_TEST_THRESHOLD, MIN_CV, MIN_VALUE,  # type: ignore
-                    Z_VALUE)
+from config import (IID_TEST_THRESHOLD, MIN_REQUIRED_TAILS,  # type: ignore
+                    NUM_IGNORE, PWCET_GRANULARITY, Z_VALUE)
 from plot import plot_cv, plot_pwcet  # type: ignore
 
 
-def ljung_box_test(et_array):
+def ljung_box_test(et_array: np.ndarray) -> bool:
     return acorr_ljungbox(et_array, lags=20).iloc[-1]['lb_pvalue'] <= IID_TEST_THRESHOLD
 
 
-def kolmogorov_smirnov_test(et_array):
+def kolmogorov_smirnov_test(et_array: np.ndarray) -> bool:
     half_size = int(len(et_array) / 2)
     return kstest(et_array[:half_size], et_array[half_size:],
                   alternative='less').pvalue <= IID_TEST_THRESHOLD
 
 
-def main(et_array, title: str, plot_pwcet_flag: bool, plot_cv_flag: bool) -> None:
+def main(et_array: np.ndarray, title: str, plot_pwcet_flag: bool, plot_cv_flag: bool) -> None:
     half_size = int(len(et_array) / 2)
     et_array_sorted = np.sort(et_array)[::-1]
 
-    # Calculate CV
-    cv = np.zeros(half_size)
-    for i in range(half_size, MIN_VALUE, -1):
-        slice_ = et_array_sorted[:i] - et_array_sorted[i-1]
-        cv[MIN_VALUE + half_size - i] = np.sqrt(np.var(slice_)) / np.mean(slice_)
+    # Calculate empirical residual CV
+    residual_cv = np.zeros(half_size)
+    for i in range(len(residual_cv)):
+        threshold = et_array_sorted[i]
+        excesses = et_array_sorted[:i + 1] - threshold
+        residual_cv[i] = np.std(excesses) / np.mean(excesses)
 
-    # Calculate confidence interval
-    limit_above = np.zeros(half_size - MIN_VALUE)
-    limit_below = np.zeros(half_size - MIN_VALUE)
-    for i in range(half_size - MIN_VALUE):
-        sqrt_val = np.sqrt(half_size - i)
-        limit_above[i] = 1 + Z_VALUE / sqrt_val
-        limit_below[i] = 1 - Z_VALUE / sqrt_val
+    # Calculate acceptance region of Gumbel distribution (95% confidence interval)
+    sqrt_vals = np.sqrt(np.arange(half_size))
+    limit_above = 1 + Z_VALUE / sqrt_vals
+    limit_below = 1 - Z_VALUE / sqrt_vals
 
+    # (Optional) Plot CV and acceptance region
     if plot_cv_flag:
-        plot_cv(cv, limit_above, limit_below, title)
+        plot_cv(
+            residual_cv[NUM_IGNORE:],
+            limit_above[NUM_IGNORE:],
+            limit_below[NUM_IGNORE:],
+            title
+        )
 
-    start_pos = 0
-    for i in range(half_size - MIN_VALUE):
-        if cv[MIN_VALUE + i] > limit_above[i]:
-            start_pos = i
-
-    if start_pos > (half_size - MIN_CV):
-        print("[ERROR] Analysis not completed due to insufficient data")
-        return
-
-    best_cv = 1000.0
-    num_elements = 0
-    for i in range(start_pos, half_size - MIN_CV):
-        if (abs(cv[MIN_VALUE + i] - 1) < abs(best_cv - 1)) and (cv[MIN_VALUE + i] < limit_above[i]):
-            best_cv = cv[MIN_VALUE + i]
-            num_elements = half_size - i
-
-    # cv(th) exponentiality test
-    for i in range(half_size - MIN_CV, half_size - MIN_VALUE):
-        if cv[MIN_VALUE + i] > limit_above[i]:
-            print("[ERROR] Analysis not completed due to insufficient data")
+    # Test exponentiality for 10 < N_{+th} <= 50
+    for cv, la in zip(residual_cv[NUM_IGNORE:MIN_REQUIRED_TAILS+1],
+                      limit_above[NUM_IGNORE:MIN_REQUIRED_TAILS+1]):
+        if cv > la:
+            print("[ERROR] Please increase the number of samples.")
             return
 
+    # Find the best threshold
+    best_cv = sys.maxsize
+    num_tails = MIN_REQUIRED_TAILS
+    for i in range(MIN_REQUIRED_TAILS+1, len(residual_cv)):
+        if residual_cv[i] > limit_above[i]:  # Test exponentiality
+            break
+        if abs(residual_cv[i] - 1) < abs(best_cv - 1):
+            best_cv = residual_cv[i]
+            num_tails = i
+    threshold = et_array_sorted[num_tails - 1]
+
+    # Fitting
+    excesses = et_array_sorted[:num_tails] - threshold
+    lambda_ = 1 / np.mean(excesses)
+    estimated_et_array = np.linspace(0, np.max(excesses) + threshold, PWCET_GRANULARITY)
+    prob_cdf = expon.cdf(estimated_et_array, scale=1/lambda_)
+    estimated_et_array += threshold
+    pwcet_curve = np.column_stack((estimated_et_array, prob_cdf))
+    pwcet_curve[0, 0] = 0
+
+    # (Optional) Plot pWCET CDF
     if plot_pwcet_flag:
-        plot_pwcet(et_array, et_array_sorted, num_elements, title)
+        plot_pwcet(pwcet_curve, et_array, title)
 
     print("[INFO] Analysis completed.")
 
@@ -72,34 +83,35 @@ def main(et_array, title: str, plot_pwcet_flag: bool, plot_cv_flag: bool) -> Non
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--latency_cache_dir', type=str, help='Path to the latency cache directory',
-        default='~/autoware_optimization_tools/latency_cache'
+        '--latency_dir', default='/home/atsushi/MBPTA/sample'
     )
     parser.add_argument(
-        '--plot_pwcet', action='store_true',
-        help='Generate the pWCET plot if set', default=True
+        '--plot_pwcet', action='store_true', default=True
     )
     parser.add_argument(
-        '--plot_cv', action='store_true',
-        help='Generate the CV plot if set', default=False
+        '--plot_cv', action='store_true', default=True
     )
     args = parser.parse_args()
 
-    for cache_file in os.listdir(args.latency_cache_dir):
-        latencies = np.ceil(
-            np.load(os.path.join(args.latency_cache_dir, cache_file)) * 10**(-3)
-        ).astype(int)  # ns -> us
+    for latency_file in os.listdir(args.latency_dir):
+        if latency_file.endswith('.npy'):
+            latencies = np.load(os.path.join(args.latency_dir, latency_file))
+        elif latency_file.endswith('.txt'):
+            with open(os.path.join(args.latency_dir, latency_file), 'r') as f:
+                latencies = np.array(f.readlines(), dtype=int)
+        else:
+            raise NotImplementedError
 
         if len(latencies) < 100:
-            print(f'[ERROR] Data size is less than 100: {cache_file}')
+            print(f'[ERROR] Data size is less than 100: {latency_file}')
             continue
-        # if ljung_box_test(latencies):
-        #     print(f'[ERROR] Ljung-Box test failed: {cache_file}')
-        #     continue
-        # if kolmogorov_smirnov_test(latencies):
-        #     print(f'[ERROR] Kolmogorov-Smirnov test failed: {cache_file}')
-        #     continue
+        if ljung_box_test(latencies):
+            print(f'[ERROR] Ljung-Box test failed: {latency_file}')
+            continue
+        if kolmogorov_smirnov_test(latencies):
+            print(f'[ERROR] Kolmogorov-Smirnov test failed: {latency_file}')
+            continue
 
         main(
-            latencies, f'{cache_file.split(".")[0]}', args.plot_pwcet, args.plot_cv
+            latencies, f'{latency_file.split(".")[0]}', args.plot_pwcet, args.plot_cv
         )
--- src/plot.py
+++ src/plot.py
@@ -1,86 +1,37 @@
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
-from scipy.stats import expon  # type: ignore
-
-from config import MIN_VALUE  # type: ignore
 
 OUTPUT_DIR = '../output/'
 
 
-def plot_cv(cv, limit_above, limit_below, title) -> None:
+def plot_cv(residual_cv: np.ndarray, limit_above: np.ndarray, limit_below: np.ndarray, title: str) -> None:
     plt.figure(figsize=(10, 7.5))
-    plt.plot(cv[MIN_VALUE:], 'b-', linewidth=2)
-    plt.plot(range(len(limit_above)), limit_above, 'r-', linewidth=2)
-    plt.plot(range(len(limit_below)), limit_below, 'r-', linewidth=2)
+    plt.plot(residual_cv[::-1], 'b-', linewidth=2)
+    plt.plot(range(len(limit_above)), limit_above[::-1], 'r--', linewidth=2)
+    plt.plot(range(len(limit_below)), limit_below[::-1], 'r--', linewidth=2)
 
-    plt.xlim(0, len(cv))
+    plt.xlim(-5, len(residual_cv)+5)
     plt.ylim(0.3, 1.7)
     plt.title(title, fontsize=16)
+    plt.xlabel('Rejected Samples', fontsize=14)
+    plt.ylabel('CV', fontsize=14)
     plt.savefig(f'{OUTPUT_DIR}/{title}_cv.png')
     plt.close()
 
 
-def plot_pwcet_original(et_array, et_array_sorted, num_elements, title) -> None:
-    excesses = et_array_sorted[:num_elements] - et_array_sorted[num_elements - 1]
-    rate = 1 / np.mean(excesses)
-    rank = np.linspace(0, 20 * np.max(excesses), 1000000)
-    prob_ccdf = 1 - expon.cdf(rank, scale=1/rate)
-    rank += et_array_sorted[num_elements - 1]
-    pwcet_curve = np.column_stack((rank, prob_ccdf))
-
-    # Generating ECCDF data
-    rankeccdf = np.linspace(1.0, 0.0, len(et_array))
-    traceeccdf = np.sort(et_array)
-    eccdf1 = np.column_stack((traceeccdf, rankeccdf))
-
-    # Adjusting pWCETcurve's first value
-    pwcet_curve[0, 0] = 0
-
-    # Setting up the plot
-    plt.figure(figsize=(10, 6.25))
-    y_range = [1e-16, 1]
-    x_range = [np.nanmin(et_array) * 0.9, 2500]
-
-    # Plotting the curves
-    plt.plot(pwcet_curve[:, 0], pwcet_curve[:, 1], 'k-', linewidth=4, label=title)
-    plt.plot(eccdf1[:, 0], eccdf1[:, 1], 'r--', linewidth=3)
-
-    # Adding horizontal lines for probability thresholds
-    plt.axhline(y=1e-3, color='black', linestyle='--', linewidth=1)
-    plt.axhline(y=1e-6, color='black', linestyle='--', linewidth=1)
-    plt.axhline(y=1e-9, color='black', linestyle='--', linewidth=1)
-    plt.axhline(y=1e-12, color='black', linestyle='--', linewidth=1)
-
-    plt.yscale('log')
-    plt.xlim(x_range)
-    plt.ylim(y_range)
-    plt.title(title, fontsize=16)
-    plt.savefig(f'{title}_pWCET.png')
-    plt.close()
-
-
-def plot_pwcet(et_array, et_array_sorted, num_elements, title) -> None:
-    # Calculating the pWCET curve
-    excesses = et_array_sorted[:num_elements] - et_array_sorted[num_elements - 1]
-    rate = 1 / np.mean(excesses)
-    rank = np.linspace(0, 20 * np.max(excesses), 1000000)
-    prob_cdf = expon.cdf(rank, scale=1/rate)
-    rank += et_array_sorted[num_elements - 1]
-    pwcet_curve = np.column_stack((rank, prob_cdf))
-    pwcet_curve[0, 0] = 0
-
+def plot_pwcet(pwcet_curve: np.ndarray, et_array: np.ndarray, title: str) -> None:
     # Calculating the profiled CDF
-    et_cdf = np.sort(et_array)
-    rank_cdf = np.linspace(1/len(et_array), 1, len(et_array))
-    cdf = np.column_stack((et_cdf, rank_cdf))
+    et_array = np.sort(et_array)
+    prob_cdf = np.linspace(1/len(et_array), 1, len(et_array))
+    et_curve = np.column_stack((et_array, prob_cdf))
 
     # Plotting the CDF
     plt.figure(figsize=(10, 6))
     plt.plot(pwcet_curve[:, 0], pwcet_curve[:, 1], 'b-', linewidth=3, label='Analyzed CDF')
-    plt.plot(cdf[:, 0], cdf[:, 1], 'r--', linewidth=3, label='Profiled CDF')
+    plt.plot(et_curve[:, 0], et_curve[:, 1], 'g--', linewidth=3, label='Profiled CDF')
     plt.title(title, fontsize=16)
-    plt.xlabel('Execution Time [us]', fontsize=14)
-    plt.ylabel('Probability', fontsize=14)
+    plt.xlabel('Execution Time', fontsize=14)
+    plt.ylabel('Probability Density', fontsize=14)
     plt.legend()
     plt.grid(True)
     plt.savefig(f'{OUTPUT_DIR}/{title}_pwcet_cdf.png')
